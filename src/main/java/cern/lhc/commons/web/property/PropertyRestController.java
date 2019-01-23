4c8d2e0128f39a8df8877da4f8ad8e2e5b17e9af
Andrea Calia <andrea.calia@cern.ch>
2019-01-23 16:40:32 +0000

    using Properties for most of the settings in coupling
    git-svn-id: https://svn.cern.ch/reps/acc-co/trunk/lhc/commons/lhc-commons-web@474744 4e1e14d5-dc27-4203-babc-ce7d9a7dada2

--- src/main/java/cern/lhc/commons/web/property/PropertyRestController.java
+++ src/main/java/cern/lhc/commons/web/property/PropertyRestController.java
@@ -5,6 +5,8 @@
 package cern.lhc.commons.web.property;
 
 import cern.lhc.commons.web.rbac.RbacProtected;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,16 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
 @RequestMapping(PropertyRestController.PROPERTY_ENDPOINT)
 public class PropertyRestController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertyRestController.class);
     public static final String PROPERTY_ENDPOINT = "properties";
 
     @Autowired
     private List<RestPropertyMapping> restProperties;
+
+    @PostConstruct
+    private void init() {
+        restProperties.forEach(mapping -> LOGGER.info("Mapped rest property {}", mapping.path()));
+    }
 
     @GetMapping("{endpoint}")
     public String get(@PathVariable("endpoint") String endpoint) {
--- src/main/java/cern/lhc/commons/web/property/PropertyWebsocketConfiguration.java
+++ src/main/java/cern/lhc/commons/web/property/PropertyWebsocketConfiguration.java
@@ -1,5 +1,7 @@
 package cern.lhc.commons.web.property;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -15,6 +17,8 @@ import static java.util.stream.Collectors.toList;
 @Configuration
 public class PropertyWebsocketConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertyWebsocketConfiguration.class);
+
     @Bean
     public List<StreamWebsocketMapping> propertyWebsockets(List<RestPropertyMapping> restProperties) {
         return restProperties.stream().map(r -> new StreamWebsocketMapping(r.property().asStream(),
@@ -26,7 +30,10 @@ public class PropertyWebsocketConfiguration {
             List<List<StreamWebsocketMapping>> bulkHandlers) {
         Stream<StreamWebsocketMapping> allHandlers = concat(handlers.stream(),
                 bulkHandlers.stream().flatMap(List::stream));
-        return registry -> allHandlers.forEach(h -> registry.addHandler(websocketFromStream(h.stream()), h.path()));
+        return registry -> allHandlers.forEach(h -> {
+            LOGGER.info("Mapping websocket handler for {}", h.path());
+            registry.addHandler(websocketFromStream(h.stream()), h.path());
+        });
     }
 
 }
