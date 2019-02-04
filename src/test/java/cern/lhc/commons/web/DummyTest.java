0425488a0c8166e732335c24953094dbb25273ed
David John Walsh <david.john.walsh@cern.ch>
2019-02-04 13:57:27 +0000

    updating dependencies
    git-svn-id: https://svn.cern.ch/reps/acc-co/trunk/lhc/commons/lhc-commons-web@475393 4e1e14d5-dc27-4203-babc-ce7d9a7dada2

---
+++ src/test/java/cern/lhc/commons/web/DummyTest.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2019 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package cern.lhc.commons.web;
+
+import org.junit.Test;
+
+public class DummyTest {
+    @Test
+    public void test() {
+        /* Empty test */
+    }
+
+}
